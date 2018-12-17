478d03819c0a6c79c6bd66f6664f254c2e9ba15d
Medhat Dawoud <medhatdawoud@gmail.com>
2018-12-17 03:35:15 +0100

    3.1 Add a logging function

--- section 3/cafe/src/store.js
+++ section 3/cafe/src/store.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers'
+import { addOrder } from './actions/orderActions';
 
 const store = createStore(rootReducer);
+
+function interruptDispatch(action) {
+  console.log('before dispatching');
+  action.payload = {...action.payload, amount: 1};
+  store.dispatch(action);
+  console.log('after dispatching');
+}
+
+interruptDispatch(addOrder({order:'Tea'}));
 
 export default store; 
